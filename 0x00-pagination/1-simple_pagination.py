34b7dbfa58ba482152736a883f812f3807e3d390
Olusegun <ayosegun2001@gmail.com>
2024-08-19 19:11:05 +0100

    0x00. Pagination: Back-end

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -76,9 +76,9 @@ class Server:
 
         try:
             # Return the slice of the dataset for the requested page
-            if start_index < len(data):
+            # if start_index < len(data):
                 return data[start_index:end_index]
-            else:
-                return []
+            # else:
+                # return []
         except IndexError:
             return []
