295e3f180f9a7f3e020019b5af91218360edc476
Olusegun <ayosegun2001@gmail.com>
2024-08-20 15:19:40 +0100

    0x00. Pagination: Back-end

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -47,8 +47,7 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1,
-                 page_size: int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Get a page of the dataset.
 
